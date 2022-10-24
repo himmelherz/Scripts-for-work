60aa541b14d777a683ca176d86425b23016ee8cb
himmelherz <nozarishi@gmail.com>
2022-10-24 14:04:06 +0300

    Create info_check.sh

---
+++ checking proccesses by iterarion of reboots/info_check.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+i=0
+k=0
+prev_line=''
+cat uptime_log.txt | grep "start time:" >> checking_uptime.txt
+while read line
+do
+
+
+
+	if [ $i -eq 0 ];
+	then
+		prev_line=$line
+		let "i += 1"
+		
+	
+	else
+			let "k += 1"
+			let "i -= 1"
+			echo $prev_line
+			echo $line
+			if [ "$prev_line" != "$line" ];
+			then
+				echo "detected" >> checking_uptime.txt
+				echo $k >> checking_uptime.txt
+				
+
+
+			fi
+	fi
+		
+
+done < checking_uptime.txt
+
+exit 0
